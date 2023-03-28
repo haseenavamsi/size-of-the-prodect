f197c2b4b016a974e386197fd9a9c1f2943d5a4f
haseena vamsidhar <121420151+haseenavamsi@users.noreply.github.com>
2023-03-28 20:32:56 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,21 @@
+const express=require('express');
+const bodyParser=require('body-Parser');
+
+const app=express();
+
+app.use(bodyParser.urlencoded({extented:false}))
+ 
+app.use('/add-prodect',(req,res,next)=> {
+res.send('<form action="/prodect" method="POST"><input type="text" name="title"><button type="Submit">Add Prodect</button></from>');
+
+});
+
+app.post ('/prodect',(req,res,next)=>{
+  console.log(req.body);
+  res.redirect('/');
+});
+
+app.use('/',(req,res,next)=>{
+   res.send('<h1>Hello from Express!</h1>');
+});
+app.listen(3000);
